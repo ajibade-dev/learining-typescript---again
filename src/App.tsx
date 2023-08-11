980f7a0dcc63535d2ebdf03d2f07ae26722c6105
unknown <ajibadeifedolapo17@gmail.com>
2023-08-11 01:31:16 +0100

    Pushin p...

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+import Greet from './components/Greet';
 import './App.css';
+import Person from './components/Person';
+import PersonList from './components/PersonList';
 
 function App() {
+const personName = {
+  first: 'Paul',
+  last: 'Ajibade'
+}
+
+const nameList = [
+  {
+    first: 'Bruce',
+    last: 'Wayne'
+  },
+  {
+    first: 'Clark',
+    last: 'Kent'
+  },
+  {
+    first: 'Paul',
+    last: 'Ajibade'
+  },
+]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Greet 
+      name='Vishwas'
+      messageCount={20}
+      isLoggedIn={false}
+      />
+      <Person name={personName} />
+      <PersonList names={nameList} />
     </div>
   );
 }
